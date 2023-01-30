e943ddc020668a6dd1271db541713c2125ac3615
Darby Manning <me@darbymanning.com>
2023-01-30 14:39:47 +0000

    Add Hygraph & components/routes (#3)
    * This does not yet setup anything for preview mode. This is largely the boilerplate styles and routing

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,49 @@
+<main>
+	<slot />
+</main>
+
+<footer>
+	<div class="container">
+		<h3>Prerendered with SvelteKit.</h3>
+		<a
+			href="https://github.com/darbymanning/sveltekit-preview-mode"
+			rel="noreferrer"
+			target="_blank"
+		>
+			View on GitHub
+		</a>
+	</div>
+</footer>
+
+<style>
+	.container {
+		margin: var(--gap) auto;
+		width: min(85vw, 128rem);
+	}
+
+	footer {
+		background: #111;
+		gap: 2rem;
+		padding: var(--gap) 0;
+	}
+
+	.container {
+		align-items: center;
+		display: flex;
+		flex-flow: row wrap;
+		justify-content: space-between;
+	}
+
+	h3 {
+		font-size: clamp(3.2rem, 7vw, 4rem);
+		letter-spacing: var(--letter-inset);
+	}
+
+	a {
+		font-weight: bold;
+	}
+
+	a:not(:hover) {
+		text-decoration: none;
+	}
+</style>
---
+++ src/routes/+page.server.ts
@@ -0,0 +1,8 @@
+import cms from '$lib/cms';
+import type { PageServerLoad } from './$types';
+
+export const load = (async () => {
+	return {
+		posts: await cms.get_posts()
+	};
+}) satisfies PageServerLoad;
