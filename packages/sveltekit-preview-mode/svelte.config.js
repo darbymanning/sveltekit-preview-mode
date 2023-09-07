c66736b4d8f703c5f4e58d835f597fb6c64bde73
Darby Manning <me@darbymanning.com>
2023-09-07 11:29:58 +0100

    Update dependencies
    - Update urql to use `exchanges`
    - Update Contentful SDK (mainly impacts types)

--- packages/sveltekit-preview-mode/svelte.config.js
+++ packages/sveltekit-preview-mode/svelte.config.js
@@ -1,10 +1,10 @@
-import preprocess from "svelte-preprocess";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: preprocess(),
+  preprocess: vitePreprocess(),
 };
 
 export default config;
